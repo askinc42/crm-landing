58c51b3b7f0707c56759a3be9b104b6ab7bde2c6
Almirasun <almira.sagynbek@gmail.com>
2023-07-08 21:22:50 +0600

    added amount auto counting

--- src/components/amount/Amount.jsx
+++ src/components/amount/Amount.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import s from "./Amount.module.scss"
+import CountUp from "react-countup";
+import s from "./Amount.module.scss";
 
 const Amount = () => {
   return (
     <section>
       <div className={`${s.amountGroup} ${s.flexCenter}`}>
         <div className={`${s.amountGroupContent} ${s.flexCenter}`}>
-          <div className={s.totalAmount}>10,212,010</div>
+          <CountUp
+            className={s.totalAmount}
+            start={10195900}
+            end={10212010}
+            duration={4}
+          />
           <h3 className={s.amountTitle}>Общее количество товаров</h3>
         </div>
       </div>
